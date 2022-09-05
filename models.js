9a240c36e6135651b3b087ac4f99a2c3b3d211df
Olumide Olugbemiro <olugbemiro.olumide@gmail.com>
2022-09-05 16:37:51 +0100

    added schema for all models required

---
+++ models.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const ProfileSchema = new Schema({
+    name: String,
+    image: String,
+    mbti: String,
+    enneagram: String,
+    description: String,
+    variant: String,
+    tritype: Number,
+    socionics: String,
+    sloan: String,
+    psyche: String,
+    created: { type: Date, default: Date.now }
+});
+
+const UserSchema = new Schema({
+    name: String,
+    created: { type: Date, default: Date.now }
+});
+
+const CommentSchema = new Schema({
+    name: String,
+    title: String,
+    comment: String,
+    votes: Array,
+    profileId: mongoose.Types.ObjectId,
+    userId: mongoose.Types.ObjectId,
+    likes: { type: Number, default: 0 },
+    created: { type: Date, default: Date.now }
+});
+
+const CommentLikeSchema = new Schema({
+    name: String,
+    commentId: mongoose.Types.ObjectId,
+});
+
+const User = mongoose.model("users", UserSchema)
+const Profile = mongoose.model("profiles", ProfileSchema)
+const Comment = mongoose.model("comments", CommentSchema);
+const CommentLike = mongoose.model("comment_likes", CommentLikeSchema);
+
+module.exports = { User, Profile, Comment, CommentLike };
