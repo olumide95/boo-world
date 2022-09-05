6fe9941b3c6049bca5abc999ca39cc16ddb5e6bc
Olumide Olugbemiro <olugbemiro.olumide@gmail.com>
2022-09-05 16:08:42 +0100

    setup modo db memory server

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
 'use strict';
 
-const express = require('express');
-const app = express();
-const port =  process.env.PORT || 3000;
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const bodyParser = require('body-parser');
 
-// set the view engine to ejs
+const express = require('express');
+const mongoose = require("mongoose");
+const port = process.env.PORT || 3000;
+const app = express();
+
+async function MongooseConnect() {
+    const mongod = await MongoMemoryServer.create();
+    const uri = mongod.getUri();
+
+    mongoose.connect(uri, function(err, con) {
+        console.log("Success: ", con)
+        console.log("Failure: ", err)
+    });
+}
+
+mongoose.connection.on('error', (err) => {
+    throw new Error(`unable to connect to database: ${err}`)
+})
+
+
 app.set('view engine', 'ejs');
 
-// routes
-app.use('/', require('./routes/profile')());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-// start server
-const server = app.listen(port);
-console.log('Express started. Listening on %s', port);
+app.use('/profiles', require('./routes/profile')());
+
+const server = app.listen(port, () => {
+    console.log('Express started. Listening on %s', port);
+    MongooseConnect()
+});
+
+module.exports = app;
