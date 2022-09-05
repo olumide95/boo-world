426eb0e1ad4febdb6cabab47889567df4f4edbfe
Olumide Olugbemiro <olugbemiro.olumide@gmail.com>
2022-09-05 17:05:15 +0100

    added endpoint to create a profile

--- __tests__/profile.test.js
+++ __tests__/profile.test.js
@@ -10,5 +10,27 @@ describe("profile", () => {
         expect(response.status).toBe(404);
         expect(response.body.message).toBe('No profile with id (1) found');
     })
+
+    it("Create a profile when all required parameters are sent correctly", async() => {
+        const response = await request.post("/profiles").send({"name": "A Martinez", "description": "Adolph Larrue Martinez III.", "mbti": "ISFJ", "enneagram": "9w3", "variant": "sp/so", "tritype": 725, "socionics": "SEE", "sloan": "RCOEN", "psyche": "FEVL"})
+
+        expect(response.status).toBe(201);
+        expect(response.body.message).toBe('Profile created successfully.');
+    })
+
+    it("returns a 400 error when name param is not sent", async() => {
+        const response = await request.post("/profiles").send({title: 'Title', comment : 'comment'})
+        expect(response.status).toBe(400);
+    })
+
+    it("returns a 400 error when title param is not sent", async() => {
+        const response = await request.post("/profiles").send({name: 'John', comment : 'comment'})
+        expect(response.status).toBe(400);
+    })
+
+    it("returns a 400 error when comment param is not sent", async() => {
+        const response = await request.post("/profiles").send({name: 'John', title : 'Title'})
+        expect(response.status).toBe(400);
+    })
     
 })
--- routes/profile.js
+++ routes/profile.js
@@ -3,10 +3,11 @@
 const express = require('express');
 const router = express.Router();
 const { Profile } = require("../models.js")
+const { body,  validationResult } = require('express-validator');
 
 module.exports = function() {
 
-  router.get('/:id', async function(req, res, next) {
+  router.get('/:id', async function(req, res) {
     const { id }  = req.params 
     const profile = await Profile.findOne({})
 
@@ -22,6 +23,37 @@ module.exports = function() {
     });
 
   });
+
+  router.post('/',
+    body('name', 'Name is required').exists(),
+    body('description', 'Ddescription is required').exists(),
+    body('mbti', 'mbti is required').exists(),
+    body('variant', 'Variant is required').exists(),
+    body('tritype').exists().withMessage('Tritype is required').isNumeric().withMessage('Tritype must be a number'),
+    body('socionics', 'Socionics is required').exists(),
+    body('sloan', 'Sloan is required').exists(),
+    body('psyche', 'Psyche is required').exists(),
+    async function(req, res) {
+        
+        const errors = validationResult(req);
+ 
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array()
+            });
+        }
+            
+        const { name, description, mbti, enneagram, variant, tritype, socionics, sloan, psyche } = req.body
+        const image = 'https://soulverse.boo.world/images/1.png';
+
+        const profile = await (new Profile({name, description, mbti, enneagram, variant, tritype, socionics, sloan, psyche, image}).save())
+
+        return res.status(201).json({
+            message: "Profile created successfully.",
+            status: true,
+            profile
+        })
+    });
 
   return router;
 }
